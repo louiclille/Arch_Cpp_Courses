a41f33bd2ab769a472721e6f8c61d56c5ebf9eed
root <root@localhost.localdomain>
2020-06-13 21:46:28 +0800

    20200613

--- 1_Data_Structure/1_Sort/my_sort.cpp
+++ 1_Data_Structure/1_Sort/my_sort.cpp
@@ -2,9 +2,12 @@
 #include <assert.h>
 #include <array>
 #include <initializer_list>
+#include <boost/array.hpp>
+#include <boost/bind.hpp>
 
 typedef int type;
 
+template <class type>
 void shell_sort(type arr[], ssize_t length)
 {
     assert(arr && length > 0);
@@ -21,12 +24,50 @@ void shell_sort(type arr[], ssize_t length)
     }
 }
 
+int merge_sort(int data[], int *temp, int start, int end)
+{
+    int middle;
+}
+
+int _quick_sort(type data[], int left, int right)
+{
+    if (right < left)
+        return -1;
+    int i = left;
+    int j = right;
+    int key = data[left];
+
+    while (i != j) {
+        
+        while (i < j && key <= data[j]) {
+            --j;
+        }
+        data[i] = data[j];
+        while (i < j && key >= data[i]) {
+            ++i;
+        }
+        data[j] = data[i];
+    }
+    data[i] = key;
+    _quick_sort(data, left, i - 1);
+    _quick_sort(data, i + 1, right);
+}
+
+void quick_sort(type arr[], int length)
+{
+    _quick_sort(arr, 0, length - 1);
+}
+
+
 int main()
 {
     int arr[] {233, 34, 345, 76, 14, 374, 876, 12, 27, 764};
-    printf("Hello world\n");
-    shell_sort(arr, sizeof(arr)/sizeof(arr[0]));
-    for (auto &ele : arr) {
+    boost::array<int, 10> barr {233, 34, 345, 76, 14, 374, 876, 12, 27, 764};
+    auto func = boost::bind(&shell_sort<int>, barr.c_array(), barr.size());
+    func();
+    /*printf("Hello world\n");
+    shell_sort(arr, sizeof(arr)/sizeof(arr[0]));*/
+    for (auto &ele : barr) {
         printf("%d ", ele);
     }
     putchar('\n');
